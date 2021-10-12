a302b36724d9d0d9bfec61f4c5c186b5739a4295
zchengyun <1690132420@qq.com>
2021-10-13 01:34:30 +0800

    添加项目文件。

---
+++ load_dll.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+typedef void (*printFunc) ();
+
+int main()
+{
+    HMODULE hdll = LoadLibrary(L"dll.dll");
+    if (hdll != NULL) {
+        std::cout << "加载dll成功!" << std::endl;
+
+        printFunc print = (printFunc)GetProcAddress(hdll,"print");
+        if (print) {
+            print();
+        }
+        
+		//AddFunc add = (AddFunc)GetProcAddress(hdll, "mydll::person::getName");
+
+       //std::cout<<add()<<std::endl;
+        system("pause");
+    }
+}
